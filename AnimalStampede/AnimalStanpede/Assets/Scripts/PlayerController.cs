5488d3b46b2455d024a4e5ce20865b16f61941ce
Yanis Touaitia <touaitiay1@roverkids.org>
2025-04-14 14:18:42 -0400

    Adds basic player movement
    Adds a player controller script that allows the player to move
    horizontally and constrains the player within a defined range.
    Includes a basic game scene with environment elements.

---
+++ AnimalStampede/AnimalStanpede/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.00f;
+    public float Range = 12;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -Range)
+        {
+            transform.position = new Vector3(-Range, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > Range)
+        {
+            transform.position = new Vector3(Range, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
