56e8337f7c3addd1f76f964e941b359afac83f86
Yanis Touaitia <touaitiay1@roverkids.org>
2025-04-15 14:00:41 -0400

    Adds animal and food prefabs
    Adds prefabs for various animal types and food.
    Also renames `PlayerController` to `DestroyOutOfBounds`
    and modifies its behavior to destroy objects when they
    move out of bounds and creates a new script `MoveForward`
    for moving spawned objects forward.
    The `PlayerController` script is modified to handle player
    movement and spawning of food objects.
    A new scene "Unit 2, Version 1.2" with the created
    assets and updated scripts is created.

--- AnimalStampede/AnimalStanpede/Assets/Scripts/PlayerController.cs
+++ AnimalStampede/AnimalStanpede/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.00f;
     public float Range = 12;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +29,10 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
